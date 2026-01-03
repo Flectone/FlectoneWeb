2310a6004bbc6a881baced8f2f137c7c0c777bd7
vplend <plshchkv@gmail.com>
2026-01-04 00:20:24 +0500

    refactor: remove unused Header and Footer components from pages

--- src/app/[lang]/(main)/about/page.tsx
+++ src/app/[lang]/(main)/about/page.tsx
@@ -1,13 +1,9 @@
-import Card from "@/components/Card/Card";
 import Members from "@/components/Content/Members";
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
+
 export default function Home() {
   return (
-    <div className="flex pb-4 flex-col justify-between items-center min-h-screen">
-      <Header/>
+    <div className="flex pb-4 flex-col justify-between items-center">
       <Members/>
-      <Footer/>
     </div>
   );
 }
--- src/app/[lang]/(main)/mix/page.tsx
+++ src/app/[lang]/(main)/mix/page.tsx
@@ -1,13 +1,9 @@
-import Projects from "@/components/Content/Projects";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
 import FlectoneMix from "@/components/Content/FlectoneMix";
+
 export default function Home() {
   return (
     <div className="flex pb-4 flex-col justify-between min-h-screen max-xl:items-center">
-      <Header/>
       <FlectoneMix/>
-      <Footer/>
     </div>
   );
 }
