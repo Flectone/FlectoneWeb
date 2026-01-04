fbaaf017d9446814fc9fab71c61007de2212180e
vplend <plshchkv@gmail.com>
2026-01-04 18:30:26 +0500

    refactor: adjust image container styles and dimensions in MDX components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -14,13 +14,13 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
       const imageSrc = typeof src === 'object' && src !== null ? (src as any).src : src;
       if (!imageSrc) return null;
       return (
-        <span className="mt-2 flex justify-center items-center bg-linear-to-br from-fd-primary/10 rounded-xl border p-4">
-          <span className="relative max-w-125 w-full block">
+        <span className="mt-2 flex justify-center items-center bg-linear-to-br from-fd-primary/10 rounded-xl border p-6">
+          <span className="relative max-w-250 w-full block">
             <img
               src={imageSrc}
               alt={alt || 'image'}
               {...rest}
-              className="rounded-lg shadow-md w-full h-auto"
+              className="rounded-lg shadow-md !m-0 w-full h-auto"
               loading="lazy"
             />
           </span>
