0c1fe28105f5fe36d4f8efc8963f8587e703590e
vplend <plshchkv@gmail.com>
2026-01-07 03:17:38 +0500

    refactor: adjust `Callout` background style for improved opacity consistency

--- src/components/Pulse/Callout.tsx
+++ src/components/Pulse/Callout.tsx
@@ -26,7 +26,7 @@ const typeIcons: Record<CalloutProps["type"], ReactNode> = {
 
 export default function Callout({type, title, children}: CalloutProps) {
   return (
-    <div className={` pt-3 pr-3 pb-3 w-full flex gap-3 mb-4 bg-fd-card border rounded-lg overflow-hidden`}>
+    <div className={` pt-3 pr-3 pb-3 w-full flex gap-3 mb-4 bg-fd-card/50 border rounded-lg overflow-hidden`}>
       <div className={`${typeStyles[type]} w-0.5 ml-[6px] rounded-full shrink-0`}></div>
       <div className=''>
         <div className='flex items-center text-sm gap-2 font-semibold [&_p]:my-0'>
