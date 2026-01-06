700d989d5774c49c7e552d71583106cf9f423f12
vplend <plshchkv@gmail.com>
2026-01-06 22:41:25 +0500

    refactor: update `FlectoneChat` and `FlectoneMix` layouts with refined spacing, border styling, and button alignment adjustments

--- src/components/Content/FlectoneChat.tsx
+++ src/components/Content/FlectoneChat.tsx
@@ -12,20 +12,20 @@ export default function FlectoneChat() {
   const t = useTranslations('FlectoneChat');
 
   return (
-    <div className="flex w-full max-xl:w-2xl max-md:w-full mt-6 max-xl:flex-col ">
+    <div className="flex w-full max-xl:w-2xl max-md:w-full max-xl:flex-col ">
       <Card className="mr-4 w-sm flex flex-col justify-between max-xl:w-full max-xl:my-4">
         <div>
           <h1 className="text-2xl font-bold"><b>Flectone</b>Chat</h1>
           <p>{t('description')}</p>
           <h2 className="my-4">{t.rich('usePulse', {strong: (chunks) => <strong>{chunks}</strong>})}</h2>
         </div>
-        <div className="flex gap-2 max-lg:mt-2 xl:flex-col justify-start ">
+        <div className="flex gap-2 max-lg:mt-2 xl:flex-col justify-start">
           <LinkButton mode="green" className="flex items-center" href="https://modrinth.com/plugin/flectonepulse"><SiModrinth className="mr-1" size={'16px'}/>FlectonePulse</LinkButton>
           <LinkButton mode="gray" className="flex items-center" href="https://github.com/Flectone/FlectoneChat"><SiGithub className="mr-1" size={'16px'}/>GitHub</LinkButton>
         </div>
       </Card>
       <Card className="w-full">
-        <Svg src={t('imagePath')} className='rounded-md'/>
+        <Svg src={t('imagePath')} className='rounded-md border-[0.5px]'/>
       </Card>
     </div>
   )
