32089037c61db460d6c5e3cb5c692a8a3c98bab3
vplend <plshchkv@gmail.com>
2026-01-06 22:36:41 +0500

    refactor: refine `FlectoneChat` and `FlectoneMix` layouts with updated spacing, button alignment, and SVG usage

--- src/app/[lang]/(main)/chat/page.tsx
+++ src/app/[lang]/(main)/chat/page.tsx
@@ -2,7 +2,7 @@ import FlectoneChat from "@/components/Content/FlectoneChat";
 
 export default function Home() {
   return (
-    <div className="flex pb-4 flex-col justify-between max-xl:items-center">
+    <div className="flex py-4 w-6xl flex-col justify-center max-xl:items-center">
       <FlectoneChat/>
     </div>
   );
--- src/app/[lang]/(main)/mix/page.tsx
+++ src/app/[lang]/(main)/mix/page.tsx
@@ -2,7 +2,7 @@ import FlectoneMix from "@/components/Content/FlectoneMix";
 
 export default function Home() {
   return (
-    <div className="flex pb-4 flex-col justify-between min-h-screen max-xl:items-center">
+    <div className="flex w-6xl py-4 flex-col justify-center max-xl:items-center">
       <FlectoneMix/>
     </div>
   );
