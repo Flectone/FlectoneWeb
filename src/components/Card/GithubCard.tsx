4979c3ec1dd87927880be983ac24da3ee2962550
vplend <plshchkv@gmail.com>
2026-01-06 18:15:56 +0500

    refactor: update `GithubCard` Image styles to include rounded corners and remove redundant classes

--- src/components/Card/GithubCard.tsx
+++ src/components/Card/GithubCard.tsx
@@ -14,8 +14,8 @@ interface GithubCarpProps {
 export default function GithubCard({ className, link, name, description, avatar }: GithubCarpProps) {
     return (
         <>
-            <Card path={link} target="blank" className={`${className} gitcard max-sm:hidden mb-4 duration-100 p-4 backdrop-blur-3xl bg-(--primary) w-full rounded-xl flex`}>
-                <Image className="" width={86} height={86} alt="avatar" src={avatar} />
+            <Card path={link} target="blank" className={`${className} max-sm:hidden duration-100 p-4 bg-(--primary) w-full rounded-xl flex`}>
+                <Image className="rounded-md" width={86} height={86} alt="avatar" src={avatar} />
                 <div className="ml-4 flex flex-col justify-center">
                     <h1 className="font-semibold">{name}</h1>
                     <p>{description}</p>
@@ -23,7 +23,7 @@ export default function GithubCard({ className, link, name, description, avatar 
             </Card>
             <Card path={link} target="blank" className={`${className} ${link ? 'hover:bg-(--muted-primary) cursor-pointer' : ''} block gitcard sm:hidden h-fit mb-4 duration-100 p-8 backdrop-blur-3xl bg-(--primary) w-full rounded-xl`}>
                 <div className="flex items-center mb-4">
-                    <Image className="" width={52} height={52} alt="avatar" src={avatar} />
+                    <Image className="rounded-md" width={52} height={52} alt="avatar" src={avatar} />
                     <h1 className="font-semibold ml-4">{name}</h1>
                 </div>
                 <p>{description}</p>
