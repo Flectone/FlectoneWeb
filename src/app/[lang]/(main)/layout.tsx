21d40a96efde4ac5e68bb31e730038d8138836bb
vplend <plshchkv@gmail.com>
2026-01-06 22:41:34 +0500

    refactor: simplify background class in main layout by removing unused SVG references

--- src/app/[lang]/(main)/layout.tsx
+++ src/app/[lang]/(main)/layout.tsx
@@ -46,7 +46,7 @@ export default function Layout({children}: LayoutProps<'/[lang]'>) {
       ]}
     >
       <div
-        className='bg-size-[3rem] bg-[url(/square.svg)] not-dark:bg-[url(/squaredark.svg)] flex justify-center min-h-[calc(100vh-56px)] items-center max-xl:px-24 max-lg:px-12 max-sm:px-4'
+        className='flex justify-center min-h-[calc(100vh-56px)] items-center max-xl:px-24 max-lg:px-12 max-sm:px-4'
       >
         {children}
       </div>
