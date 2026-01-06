01a19d6f122054040ca8ab0c8c087d0cd2100742
vplend <plshchkv@gmail.com>
2026-01-07 02:39:59 +0500

    refactor: add `Callout` component to `Pulse` and integrate it into MDX components

---
+++ src/components/Pulse/Callout.tsx
@@ -0,0 +1,42 @@
+import { FaInfoCircle } from "react-icons/fa";
+import { TiWarning } from "react-icons/ti";
+import { BiSolidErrorAlt } from "react-icons/bi";
+import { IoMdBulb } from "react-icons/io";
+import {ReactNode} from "react";
+
+interface CalloutProps {
+  type: 'info' | 'warn' | 'idea' | 'error';
+  title: string;
+  children?: ReactNode;
+}
+
+const typeStyles: Record<CalloutProps["type"], string> = {
+  info: 'bg-fd-info/50',
+  warn: 'bg-fd-warning/50',
+  idea: 'bg-fd-success/50',
+  error: 'bg-fd-error/50'
+}
+
+const typeIcons: Record<CalloutProps["type"], ReactNode> = {
+  info: <FaInfoCircle className='w-[1em] text-fd-info'/>,
+  warn: <TiWarning className='w-[1em] text-fd-warning'/>,
+  idea: <IoMdBulb className='w-[1em] text-fd-success'/>,
+  error: <BiSolidErrorAlt className='w-[1em] text-fd-error'/>
+}
+
+export default function Callout({type, title, children}: CalloutProps) {
+  return (
+    <div className={` pt-3 pr-3 pb-3 w-full flex gap-3 mb-4 bg-fd-card border rounded-lg overflow-hidden`}>
+      <div className={`${typeStyles[type]} w-0.5 ml-[6px] rounded-full shrink-0`}></div>
+      <div className=''>
+        <div className='flex items-center text-sm gap-2 font-semibold [&_p]:my-0'>
+          {typeIcons[type]}
+          {title ? title : children}
+        </div>
+        <div className="[&_p]:my-0">
+          {title ? children : ''}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,9 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
-import { Step, Steps } from 'fumadocs-ui/components/steps';
-import type { MDXComponents } from 'mdx/types';
+import {Step, Steps} from 'fumadocs-ui/components/steps';
+import type {MDXComponents} from 'mdx/types';
 import * as TabsComponents from 'fumadocs-ui/components/tabs';
+import Callout from '@/components/Pulse/Callout'
+
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
@@ -9,23 +11,27 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     ...TabsComponents,
     Steps,
     Step,
+    Callout,
+    h1: (props) => (
+      <h1 {...props} className="border-b pb-3" />
+    ),
     img: (props) => {
-      const { src, alt, ...rest } = props;
-      const imageSrc = typeof src === 'object' && src !== null ? (src as {src: string}).src : src;
+      const {src, alt, ...rest} = props;
+      const imageSrc = typeof src === 'object' && src !== null ? (src as { src: string }).src : src;
       if (!imageSrc) return null;
 
       return (
-          <span className="mt-2 flex justify-center items-center bg-linear-to-br from-fd-primary/10 rounded-xl border p-4">
-            <span className="relative block">
-        <img
-            src={imageSrc}
-            alt={alt || 'image'}
-            {...rest}
-            className="rounded-lg shadow-md max-w-full h-auto"
-            loading="lazy"
-        />
-      </span>
-    </span>
+        <span className="w-full">
+          <span className="flex justify-start">
+            <img
+              src={imageSrc}
+              alt={alt || 'image'}
+              {...rest}
+              className="rounded-lg h-full !my-2"
+              loading="lazy"
+            />
+          </span>
+        </span>
       );
     },
   };
