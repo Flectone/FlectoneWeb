5ee9adfeb02fd4bcce8f3a886d88997139aecfda
vplend <plshchkv@gmail.com>
2026-01-07 04:25:48 +0500

    refactor: adjust `Callout` component structure for improved gap handling and text styling consistency

--- src/components/Pulse/Callout/Callout.tsx
+++ src/components/Pulse/Callout/Callout.tsx
@@ -28,12 +28,12 @@ export default function Callout({type, title, children}: CalloutProps) {
   return (
     <div className={` pt-3 pr-3 pb-3 w-full flex gap-3 mb-4 bg-fd-card/50 border rounded-lg overflow-hidden`}>
       <div className={`${typeStyles[type]} w-0.5 ml-[6px] rounded-full shrink-0`}></div>
-      <div className=''>
+      <div className={`flex flex-col ${title ? 'gap-2' : 'gap-0'}`}>
         <div className='flex items-center text-sm gap-2 font-semibold [&_p]:my-0'>
           {typeIcons[type]}
           {title ? title : children}
         </div>
-        <div className="[&_p]:my-0">
+        <div className="[&_p]:my-0 text-sm">
           {title ? children : ''}
         </div>
       </div>
