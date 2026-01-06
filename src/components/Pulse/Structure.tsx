000139967c900b48918aec58660deaed5197c22b
vplend <plshchkv@gmail.com>
2026-01-07 02:39:27 +0500

    refactor: simplify `Pulse.Structure` header by removing rich text usage and updating translations

--- src/components/Pulse/Structure.tsx
+++ src/components/Pulse/Structure.tsx
@@ -7,7 +7,7 @@ export default function Structure() {
 
   return (
     <div className="w-full flex items-center flex-col gap-3 h-auto">
-      <h1 className='text-2xl text-center font-bold border-b border-fd-foreground w-fit px-4 pb-2'>{t.rich('title', {b: (chunks) => <b>{chunks}</b>})}</h1>
+      <h1 className='text-2xl text-center font-bold border-b border-fd-foreground w-fit px-4 pb-2'>{t('title')}</h1>
       <Files className="flex flex-col justify-start text-start p-1 w-full">
         <div className='flex h-fit w-full flex-wrap justify-around'>
           <Folder className='h-fit' name='backups'>
