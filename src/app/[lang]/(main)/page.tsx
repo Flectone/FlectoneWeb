63c072b69d1d1ca737c2b5c588a5f3c681737d33
vplend <plshchkv@gmail.com>
2026-01-06 19:30:00 +0500

    refactor: enhance `Projects` layout with improved responsiveness, animations, and updated class structure

--- src/app/[lang]/(main)/page.tsx
+++ src/app/[lang]/(main)/page.tsx
@@ -2,7 +2,7 @@ import Projects from "@/components/Content/Projects";
 
 export default function Home() {
   return (
-    <div className="w-6xl">
+    <div className="w-6xl py-4">
       <Projects />
     </div>
   );
