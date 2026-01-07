0ad42fc4d1b9de2345b63453dcfe858a38bd40a8
TheFaser <artem.mochalin11@gmail.com>
2026-01-08 01:58:04 +0500

    fix: enable standalone output mode x2

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
