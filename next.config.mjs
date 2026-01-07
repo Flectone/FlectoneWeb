d73823f9340961ed55d78c3b3416e58c1e3573ff
TheFaser <artem.mochalin11@gmail.com>
2026-01-08 00:50:11 +0500

    refactor: remove standalone output mode from next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'standalone',
   images: {
     remotePatterns: [
       {
