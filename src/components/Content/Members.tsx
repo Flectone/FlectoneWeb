bcf84269a9c2f164bbd4f3584634bdf5bf568a01
vplend <plshchkv@gmail.com>
2026-01-07 18:27:14 +0500

    refactor: adjust responsive styles for headings and descriptions in `Members` and `Projects` components for improved readability

--- src/components/Content/Members.tsx
+++ src/components/Content/Members.tsx
@@ -4,7 +4,7 @@ export default function Members() {
     const t = useTranslations("Members")
     return (
       <div className="w-full flex-col flex gap-4">
-        <h1 className='font-bold text-4xl'>{t.rich('title', {b: (chunks) => <b>{chunks}</b>})}</h1>
+        <h1 className='font-bold text-4xl max-sm:w-1/3'>{t.rich('title', {b: (chunks) => <b>{chunks}</b>})}</h1>
         <div className="flex gap-4 max-sm:flex-col">
           <GithubCard link={t('TheFaser.link')} name={t('TheFaser.name')} description={t('TheFaser.description')} avatar={t('TheFaser.avatar')} />
           <GithubCard link={t('FunnyBars.link')} name={t('FunnyBars.name')} description={t('FunnyBars.description')} avatar={t('FunnyBars.avatar')} />
--- src/components/Content/Projects.tsx
+++ src/components/Content/Projects.tsx
@@ -7,14 +7,14 @@ export default function Projects() {
 
   return (
     <div className="w-full flex flex-col justify-center gap-4">
-      <h1 className='font-bold text-4xl'>{t.rich('title', {b: (chunks) => <b>{chunks}</b>})}</h1>
+      <h1 className='font-bold text-4xl max-sm:w-1/3'>{t.rich('title', {b: (chunks) => <b>{chunks}</b>})}</h1>
       <div className="h-max w-full flex gap-4 flex-col">
         <div className="flex gap-4">
           <Card path='/pulse' className="w-full h-full max-md:h-fit items-center flex justify-between p-10 relative">
             <div className="animate-pulse rounded-full [animation-duration:6s] blur-2xl sm:hidden absolute -right-5 bottom-0 h-full w-1/2 bg-fd-primary/20 "></div>
             <div className='sm:hidden'>
-              <h1 className='text-2xl max-md:text-xl max-sm:text-sm font-bold'>Flectone<b>Pulse</b></h1>
-              <p className='w-2/3 max-md:text-md max-sm:text-xs'>{t('Pulse.description')}</p>
+              <h1 className='text-2xl max-md:text-xl font-bold'>Flectone<b>Pulse</b></h1>
+              <p className='w-2/3 max-md:text-md'>{t('Pulse.description')}</p>
             </div>
             <h1 className='max-sm:hidden max-sm:w-1/2 max-xl:text-2xl max-md:text-xl max-sm:text-sm text-3xl w-1/3 font-bold'>{t('Pulse.description')}</h1>
             <img src="/flectonepulse2.png" className="max-sm:hidden max-xl:w-lg max-lg:w-sm max-md:w-xs w-xl z-50" alt='flectonepulse' />
@@ -28,16 +28,16 @@ export default function Projects() {
             link={'/mix'}
             className='w-1/2 max-xl:w-full items-center flex p-10'
             image={'/mix/mix.svg'}
-            title={<h1 className='text-2xl max-md:text-xl max-sm:text-sm font-bold'>Flectone<b>Mix</b></h1>}
-            description={<p className={'w-2/3 max-md:text-md max-sm:text-xs'}>{t('Mix.description')}</p>}
+            title={<h1 className='text-2xl max-md:text-xl font-bold'>Flectone<b>Mix</b></h1>}
+            description={<p className={'w-2/3 max-md:text-md'}>{t('Mix.description')}</p>}
             imagePosition={'right'}
           />
           <FeatureCard
             link={'/chat'}
             className='w-1/2 max-xl:w-full items-center flex p-10'
             image={t('Chat.imagePath')}
-            title={<h1 className='text-2xl max-md:text-xl max-sm:text-sm font-bold'>Flectone<b>Chat</b></h1>}
-            description={<p className={'w-2/3 max-md:text-md max-sm:text-xs'}>{t('Chat.description')}</p>}
+            title={<h1 className='text-2xl max-md:text-xl font-bold'>Flectone<b>Chat</b></h1>}
+            description={<p className={'w-2/3 max-md:text-md'}>{t('Chat.description')}</p>}
             imagePosition={'right'}
           />
         </div>
