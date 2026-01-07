d0f4420ffab483d844e8909129f8a4e62986250f
vplend <plshchkv@gmail.com>
2026-01-07 18:26:41 +0500

    refactor: add backdrop blur effect to metrics container for enhanced visual design

--- src/components/Pulse/Metrics.tsx
+++ src/components/Pulse/Metrics.tsx
@@ -5,7 +5,7 @@ export default function Metrics() {
   return(
     <div className='flex flex-col gap-3 justify-center items-center'>
       <h1 className='text-2xl w-fit text-center font-bold border-b border-fd-foreground px-4 pb-2'>{t('title')}</h1>
-      <div className="w-full flex justify-center items-center bg-linear-to-br from-fd-primary/10 rounded-xl border p-4">
+      <div className="backdrop-blur-2xl w-full flex justify-center items-center bg-linear-to-br from-fd-primary/10 rounded-xl border p-4">
         <img src="https://flectone.net/api/pulse/metrics/svg" alt="metrics" className="not-dark:invert" />
       </div>
     </div>
