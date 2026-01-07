334e9112d74e5fec6d5190c78b6e79daf11c99ca
vplend <plshchkv@gmail.com>
2026-01-07 18:26:35 +0500

    refactor: add conditional background images to layouts for light and dark modes

--- src/app/[lang]/(main)/layout.tsx
+++ src/app/[lang]/(main)/layout.tsx
@@ -46,7 +46,7 @@ export default function Layout({children}: LayoutProps<'/[lang]'>) {
       ]}
     >
       <div
-        className='flex justify-center min-h-[calc(100vh-56px)] items-center max-xl:px-24 max-lg:px-12 max-sm:px-4'
+        className='not-dark:bg-[url(/square.svg)] bg-[url(/squaredark.svg)] flex justify-center min-h-[calc(100vh-56px)] items-center max-xl:px-24 max-lg:px-12 max-sm:px-4'
       >
         {children}
       </div>
--- src/app/[lang]/pulse/(home)/layout.tsx
+++ src/app/[lang]/pulse/(home)/layout.tsx
@@ -50,7 +50,7 @@ export default function Layout({ children }: LayoutProps<'/[lang]/pulse'>) {
         }
       ]}
     >
-      <div className='flex justify-center max-xl:px-24 max-lg:px-12 max-sm:px-4'>
+      <div className='not-dark:bg-[url(/square.svg)] bg-[url(/squaredark.svg)] flex justify-center max-xl:px-24 max-lg:px-12 max-sm:px-4'>
         {children}
       </div>
     </ HomeLayout>)
