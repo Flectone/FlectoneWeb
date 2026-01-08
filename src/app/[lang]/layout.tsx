b9c71b0d6e280644ab9172b04fed542244393d3f
vplend <plshchkv@gmail.com>
2026-01-08 18:07:47 +0500

    fix: update middleware matcher to exclude `/search`, adjust layout to pass search API config

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,6 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
+import {RootProvider} from 'fumadocs-ui/provider/next';
 import '../globals.css';
-import { Inter } from 'next/font/google';
+import {Inter} from 'next/font/google';
 import {NextIntlClientProvider} from 'next-intl';
 import {source} from "@/lib/source";
 
@@ -9,65 +9,70 @@ const inter = Inter({
 });
 
 const languageNames = {
-    ru: {
-        ru: 'Русский',
-        en: 'Английский'
-    },
-    en: {
-        ru: 'Russian',
-        en: 'English'
-    }
+  ru: {
+    ru: 'Русский',
+    en: 'Английский'
+  },
+  en: {
+    ru: 'Russian',
+    en: 'English'
+  }
 };
 
 const myTranslations = {
-    ru: {
-        chooseLanguage: 'Выбрать язык',
-        toc: 'На этой странице',
-        search: 'Поиск',
-        lastUpdate: 'Последнее обновление',
-        editOnGithub: 'Редактировать на GitHub',
-        nextPage: 'Следующая страница',
-        previousPage: 'Предыдущая страница'
-    }
+  ru: {
+    chooseLanguage: 'Выбрать язык',
+    toc: 'На этой странице',
+    search: 'Поиск',
+    lastUpdate: 'Последнее обновление',
+    editOnGithub: 'Редактировать на GitHub',
+    nextPage: 'Следующая страница',
+    previousPage: 'Предыдущая страница'
+  }
 };
 
 export async function generateStaticParams() {
-    return source.generateParams();
+  return source.generateParams();
 }
 
-export default async function Layout({ params, children }: {
-    params: Promise<{ lang: string }>;
-    children: React.ReactNode
+export default async function Layout({params, children}: {
+  params: Promise<{ lang: string }>;
+  children: React.ReactNode
 }) {
-    const { lang } = await params;
+  const {lang} = await params;
 
-    const currentLang = (lang === 'ru' || lang === 'en' ? lang : 'en') as 'ru' | 'en';
+  const currentLang = (lang === 'ru' || lang === 'en' ? lang : 'en') as 'ru' | 'en';
 
-    const localeNames = [
-        {
-            name: languageNames[currentLang].ru,
-            locale: 'ru'
-        },
-        {
-            name: languageNames[currentLang].en,
-            locale: 'en'
-        }
-    ];
-    return (
-      <html className={inter.className} suppressHydrationWarning>
-          <body className="flex flex-col min-h-screen">
-              <NextIntlClientProvider>
-                  <RootProvider
-                      i18n={{
-                          locale: lang,
-                          locales: localeNames,
-                          translations: myTranslations[lang as keyof typeof myTranslations]
-                      }}
-                  >
-                      {children}
-                  </RootProvider>
-              </NextIntlClientProvider>
-          </body>
-      </html>
-    );
+  const localeNames = [
+    {
+      name: languageNames[currentLang].ru,
+      locale: 'ru'
+    },
+    {
+      name: languageNames[currentLang].en,
+      locale: 'en'
+    }
+  ];
+  return (
+    <html className={inter.className} suppressHydrationWarning>
+    <body className="flex flex-col min-h-screen">
+    <NextIntlClientProvider>
+      <RootProvider
+        search={{
+          options: {
+            api: '/search'
+          }
+        }}
+        i18n={{
+          locale: lang,
+          locales: localeNames,
+          translations: myTranslations[lang as keyof typeof myTranslations]
+        }}
+      >
+        {children}
+      </RootProvider>
+    </NextIntlClientProvider>
+    </body>
+    </html>
+  );
 }
