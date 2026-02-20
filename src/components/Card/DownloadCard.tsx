eba658ac1070e4dd7305f44281e378083f2f50fa
vplend <plshchkv@gmail.com>
2026-02-20 15:56:16 +0500

    feat: add download page

---
+++ src/components/Card/DownloadCard.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { ReactNode } from "react";
+import LinkButton from "../Button/LinkButton";
+
+interface DownloadCardProps {
+    link: string
+    icon: ReactNode
+    name: string
+    color?: 'blue' | 'green'
+    release: string
+}
+
+export default function DownloadCard({ link, icon, name, color = 'blue', release }: DownloadCardProps) {
+    return (
+        <span className={`flex w-full p-3 text-fd-foreground bg-fd-article/40 not-dark:bg-fd-article rounded-xl items-center justify-between`}>
+            <div className={`flex items-center gap-2`}>
+                {icon}
+                <div className="flex flex-col items-start">
+                    <h1 className={`font-bold`}>{name}</h1>
+                    <p className="text-sm">{release}</p>
+                </div>
+            </div>
+            <LinkButton className="rounded-md" href={link} mode={color}>Скачать</LinkButton>
+        </span>
+    )
+}
