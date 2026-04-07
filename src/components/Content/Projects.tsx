dc4ce8786b827535d34606e303cd023022c9e19e
vplend <plshchkv@gmail.com>
2026-04-08 02:42:29 +0500

    feat: add FlectoneTools card

--- src/components/Content/Projects.tsx
+++ src/components/Content/Projects.tsx
@@ -22,18 +22,14 @@ export default function Projects() {
               <Image width={580} height={120} src="/features.png" alt="metrics" className="h-full w-full rounded-xl shadow-sm" />
             </div>
           </Card>
-          {/* <Card path='/pulse' className="w-full h-full max-md:h-fit items-center flex justify-between p-10 relative">
-            <div className="animate-pulse rounded-full [animation-duration:6s] blur-2xl sm:hidden absolute -right-5 bottom-0 h-full w-1/2 bg-fd-primary/20 "></div>
-            <div className='sm:hidden'>
-              <h1 className='text-2xl max-md:text-xl font-bold'>Flectone<b>Pulse</b></h1>
-              <p className='w-2/3 max-md:text-md'>{t('Pulse.description')}</p>
-            </div>
-            <h1 className='max-sm:hidden max-sm:w-1/2 max-xl:text-2xl max-md:text-xl max-sm:text-sm text-3xl w-1/3 font-bold'>{t('Pulse.description')}</h1>
-            <Image width={700} height={400} src="/flectonepulse2.png" className="max-sm:hidden max-xl:w-lg max-lg:w-sm max-md:w-xs w-xl z-50" alt='flectonepulse' />
-            <div className="max-sm:hidden w-3/5 bottom-0 right-0 mask-[linear-gradient(to_left,white,transparent)] absolute">
-              <Image width={580} height={120} src="/features.png" alt="metrics" className="h-full w-full rounded-xl shadow-sm" />
-            </div>
-          </Card> */}
+          <FeatureCard
+            link={'/tools'}
+            className='w-full items-center flex p-10'
+            image={''}
+            title={<h1 className='text-2xl max-md:text-xl font-bold'>Flectone<b>Tools</b></h1>}
+            description={<p className={'w-full max-md:text-md'}>{t('Tools.description')}</p>}
+            imagePosition={'right'}
+          />
         </div>
         <div className="flex w-full max-xl:flex-col gap-4">
           <FeatureCard
