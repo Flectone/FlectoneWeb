30eea36fad4f8da90614e1bbed5c9c2c713db8c8
vplend <plshchkv@gmail.com>
2026-04-20 22:12:45 +0500

    refactor(TextureGenerator): combine previews into one block

--- src/components/Tools/TextureGenerator.tsx
+++ src/components/Tools/TextureGenerator.tsx
@@ -175,51 +175,53 @@ export const TextureGenerator = () => {
     return (
         <div className="flex flex-col w-full gap-4 relative">
             <div className="flex flex-col gap-4">
-                <div className='bg-[url("/assets/minecraftjungle.png")] bg-bottom flex transition-transform overflow-hidden justify-center items-center w-full p-8 rounded-2xl border-2'>
-                    <MinecraftTab tabText={isProcessingPreview ? <LoaderCircle className='size-[0.8em] animate-spin text-fd-primary' /> : <Preview inputLabel={t('input')} inputClassName='w-44' inputActiveLabel={t('drag')} />} />
-                </div>
-                <div className='font-[Minecraft] bg-[url("/assets/minecraftjungle2.png")] bg-center bg-cover pt-8 flex relative overflow-hidden transition-transform justify-start items-center w-full rounded-2xl border-2'>
-                    <div className='flex flex-col pl-1 w-fit pr-16 h-fit mb-5 bg-black/60 items-start justify-end gap-2'>
-                        <div className='flex items-end gap-1'>
-                            {isProcessingPreview ? <LoaderCircle className='size-[0.8em] animate-spin text-fd-primary' /> : <Preview inputClassName='py-1.5' />}
-                            <div className='flex items-center gap-1 h-3.5'>
-                                <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>TheFaser:</p>
-                                <p className='text-white! [text-shadow:1.2px_1.2px_0px_#252525]'>hello</p>
-                            </div>
-                        </div>
-                        <div className='flex items-end gap-1'>
-                            <div className='flex items-center gap-1 h-3.5'>
-                                <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>vpllll:</p>
-                                <p className='text-white! [text-shadow:1.2px_1.2px_0px_#252525]'>hi</p>
-                            </div>
-                        </div>
-                        <div className='flex items-end gap-1'>
-                            <div className='flex items-center gap-1 h-3.5'>
-                                <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>Terrona:</p>
-                                <p className='text-white! [text-shadow:1.2px_1.2px_0px_#252525]'>qq</p>
-                            </div>
-                        </div>
-                        <div className='flex items-start gap-1'>
-                            <div className='flex items-center gap-1 h-3.5'>
-                                <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>Realepi_Bars_:</p>
-                            </div>
-                            {isProcessingPreview ? <LoaderCircle className='size-[0.8em] animate-spin text-fd-primary' /> : <Preview inputClassName='py-1.5' />}
-                        </div>
+                <div className='font-[Minecraft] bg-[url("/assets/minecraftjungle.png")] bg-center bg-cover pt-4 relative flex flex-col rounded-2xl border-2 overflow-hidden'>
+                    <div className='w-full h-fit flex justify-center pb-12'>
+                        <MinecraftTab tabText={isProcessingPreview ? <LoaderCircle className='size-[0.8em] animate-spin text-fd-primary' /> : <Preview inputLabel={t('input')} inputClassName='w-44' inputActiveLabel={t('drag')} />} />
                     </div>
-                    <div className='flex w-full h-4 absolute bottom-0 bg-black/60 pb-2'></div>
+                    <div className='w-full'>
+                        <div className='flex flex-col pl-1 w-fit pr-16 h-fit mb-5 bg-black/60 items-start justify-end gap-2'>
+                            <div className='flex items-end gap-1'>
+                                {isProcessingPreview ? <LoaderCircle className='size-[0.8em] animate-spin text-fd-primary' /> : <Preview inputClassName='py-1.5' />}
+                                <div className='flex items-center gap-1 h-3.5'>
+                                    <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>TheFaser:</p>
+                                    <p className='text-white! [text-shadow:1.2px_1.2px_0px_#252525]'>hello</p>
+                                </div>
+                            </div>
+                            <div className='flex items-end gap-1'>
+                                <div className='flex items-center gap-1 h-3.5'>
+                                    <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>vpllll:</p>
+                                    <p className='text-white! [text-shadow:1.2px_1.2px_0px_#252525]'>hi</p>
+                                </div>
+                            </div>
+                            <div className='flex items-end gap-1'>
+                                <div className='flex items-center gap-1 h-3.5'>
+                                    <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>Terrona:</p>
+                                    <p className='text-white! [text-shadow:1.2px_1.2px_0px_#252525]'>qq</p>
+                                </div>
+                            </div>
+                            <div className='flex items-start gap-1'>
+                                <div className='flex items-center gap-1 h-3.5'>
+                                    <p className='[text-shadow:1.2px_1.2px_0px_#212F38] text-[#ABD5E3]!'>Realepi_Bars_:</p>
+                                </div>
+                                {isProcessingPreview ? <LoaderCircle className='size-[0.8em] animate-spin text-fd-primary' /> : <Preview inputClassName='py-1.5' />}
+                            </div>
+                        </div>
+                        <div className='flex w-full h-4 absolute bottom-0 bg-black/60 pb-2'></div>
+                    </div>
                 </div>
 
                 <div className='w-full flex justify-end items-center gap-2'>
                     <button
                         type='button'
                         onClick={handleStartGeneration}
-                        disabled={isProcessingPreview|| !selectedFile}
+                        disabled={isProcessingPreview || !selectedFile}
                         className={`
                             w-fit px-5 py-1 rounded-lg text-nowrap 
                             duration-100 cursor-pointer flex font-medium items-center
                             ${isProcessingPreview || !selectedFile
-                            ? 'bg-gray-400 text-gray-600 cursor-not-allowed opacity-70'
-                            : 'bg-fd-primary hover:bg-fd-muted-primary shadow-md hover:shadow-lg text-fd-primary-foreground'}
+                                ? 'bg-gray-400 text-gray-600 cursor-not-allowed opacity-70'
+                                : 'bg-fd-primary hover:bg-fd-muted-primary shadow-md hover:shadow-lg text-fd-primary-foreground'}
                         `}
                     >
                         {isProcessingPreview ? (
