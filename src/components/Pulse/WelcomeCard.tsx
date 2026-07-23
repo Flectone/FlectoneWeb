53ad058820cf58b62500b7850339d9ab7ee57f1d
vplend <plshchkv@gmail.com>
2026-07-23 23:24:34 +0500

    refactor(pulse): update image dimensions in WelcomeCard

--- src/components/Pulse/WelcomeCard.tsx
+++ src/components/Pulse/WelcomeCard.tsx
@@ -9,10 +9,8 @@ export default function WelcomeCard() {
   const t = useTranslations('Pulse')
     
   return (
-    <div
-      className='w-full max-lg:p-8 rounded-2xl bg-size-[36px] border overflow-hidden bg-fd-card/85 backdrop-blur-3xl flex px-16 py-12 justify-between items-center gap-8'>
-      <div
-        className='blur-2xl bg-fd-primary/20 -z-1 w-180 h-[calc(100%+2rem)] rounded-full absolute -right-[20%] animate-pulse [animation-duration:6s]'></div>
+    <div className='w-full max-lg:p-8 rounded-2xl bg-size-[36px] border overflow-hidden bg-fd-card/85 backdrop-blur-3xl flex px-16 py-12 justify-between items-center gap-8'>
+      <div className='blur-2xl bg-fd-primary/20 -z-1 w-180 h-[calc(100%+2rem)] rounded-full absolute -right-[20%] animate-pulse [animation-duration:6s]'></div>
       <div className='flex flex-col gap-4'>
         <h1 className='text-3xl w-2/3 max-md:w-full text-start font-bold'>{t.rich('WelcomeCard.slogan', {
           b: (chunks) => <b>{chunks}</b>
@@ -29,8 +27,8 @@ export default function WelcomeCard() {
           </ModalButton>
         </div>
       </div>
-      <Image className='max-lg:hidden w-85 rounded-xl' src={'/assets/flectonepulse/flectonepulse_title.webp'} alt={'flectonepulse'} width={1000}
-        height={1000} />
+      <Image className='max-lg:hidden w-85 rounded-xl' src={'/assets/flectonepulse/flectonepulse_title.webp'} alt={'flectonepulse'} width={500}
+        height={80} />
     </div>
   )
 }
