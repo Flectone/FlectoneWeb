8bef26469d4d67205860f79926a468d93bdf92d3
vplend <plshchkv@gmail.com>
2026-08-06 05:11:04 +0500

    refactor(vault): simplify sort component structure and improve button layout

--- src/components/Navigation/Sort.tsx
+++ src/components/Navigation/Sort.tsx
@@ -21,41 +21,27 @@ export default function Sort({
   const t = useTranslations("Header.Search");
 
   return (
-    <div className="relative flex gap-4 w-fit">
-      <button
-        className="flex items-center cursor-pointer bg-fd-card text-sm rounded-lg h-8 w-8 justify-center gap-2 text-fd-muted-foreground hover:bg-fd-accent"
-        onClick={() => setIsOpen((prev) => !prev)}
+    <div className="relative flex gap-2 w-fit">
+      <div
+        className="text-nowrap flex bg-fd-card h-8 p-1 gap-1 rounded-lg"
+        onClick={() => setIsOpen(false)}
       >
-        <Funnel size="1em" />
-      </button>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.div
-            className="absolute text-nowrap flex top-0 left-full z-1 ml-2 bg-fd-card h-8 p-1 gap-1 rounded-lg"
-            initial={{ opacity: 0, x: -10 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -10 }}
-            transition={{ duration: 0.1, ease: "easeInOut" }}
-            onClick={() => setIsOpen(false)}
+        <button
+          onClick={onClick}
+          className="transition cursor-pointer px-2 text-left rounded-md text-fd-muted-foreground hover:bg-fd-accent text-xs"
+        >
+          <ArrowDownUp size={"1em"} />
+        </button>
+        {options.map((item) => (
+          <button
+            className={`transition cursor-pointer px-3 w-full text-left rounded-md ${currentSort === item.value ? "bg-fd-accent text-fd-foreground!" : "text-fd-muted-foreground"} hover:bg-fd-accent text-xs`}
+            key={item.value}
+            onClick={() => onChange(item.value)}
           >
-            <button
-              onClick={onClick}
-              className="transition cursor-pointer px-2 text-left rounded-md text-fd-muted-foreground hover:bg-fd-accent text-xs"
-            >
-              <ArrowDownUp size={"1em"} />
-            </button>
-            {options.map((item) => (
-              <button
-                className={`transition cursor-pointer px-3 w-full text-left rounded-md ${currentSort === item.value ? "bg-fd-accent text-fd-foreground!" : "text-fd-muted-foreground"} hover:bg-fd-accent text-xs`}
-                key={item.value}
-                onClick={() => onChange(item.value)}
-              >
-                {item.label}
-              </button>
-            ))}
-          </motion.div>
-        )}
-      </AnimatePresence>
+            {item.label}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
