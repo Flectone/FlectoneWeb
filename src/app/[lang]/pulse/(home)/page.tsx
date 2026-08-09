a2120782860fe7e31f9de3ca84f42921416c4507
vplend <plshchkv@gmail.com>
2026-08-09 19:14:47 +0500

    fix(pulse): change translation keys in HomePage component

--- src/app/[lang]/pulse/(home)/page.tsx
+++ src/app/[lang]/pulse/(home)/page.tsx
@@ -1,28 +1,28 @@
-import Feautures from '@/components/Content/Pulse/Features';
-import WelcomeCard from '@/components/Content/Pulse/WelcomeCard';
+import Feautures from "@/components/Content/Pulse/Features";
+import WelcomeCard from "@/components/Content/Pulse/WelcomeCard";
 import { createMetadata } from "@/lib/create-metadata";
 import Metric from "@/components/Metric/Metric";
-import {useTranslations} from "next-intl";
+import { useTranslations } from "next-intl";
 
 export const generateMetadata = createMetadata({
-  namespace: 'Pulse'
+  namespace: "Pulse",
 });
 
 export default function HomePage() {
-  const t = useTranslations('Pulse.Metrics');
+  const t = useTranslations("Pulse.Metrics.labels");
 
   return (
     <div className="my-4 w-full max-w-6xl flex gap-8 flex-col justify-start items-center text-center">
       <WelcomeCard />
       <Feautures />
-      <div className='flex flex-col gap-3 justify-center items-center w-full'>
+      <div className="flex flex-col gap-3 justify-center items-center w-full">
         <Metric
           className="backdrop-blur-2xl py-4 bg-fd-card"
           slice={true}
           type="two-line"
           data={{
-              first: { name: t('serversCount'), apiPath: 'serverCount' },
-              second: { name: t('playersCount'), apiPath: 'playerCount' }
+            first: { name: t("serverCount"), apiPath: "serverCount" },
+            second: { name: t("playerCount"), apiPath: "playerCount" },
           }}
         />
       </div>
