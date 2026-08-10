eb9894465a644c31498eb7780987dab7802469a9
vplend <plshchkv@gmail.com>
2026-08-10 19:37:07 +0500

    refactor(pulse): remove metrics tag in search component

--- src/components/Search/SearchComponent.tsx
+++ src/components/Search/SearchComponent.tsx
@@ -1,30 +1,31 @@
-'use client';
-import { usePathname } from 'next/navigation';
-import { useDocsSearch } from 'fumadocs-core/search/client';
+"use client";
+import { usePathname } from "next/navigation";
+import { useDocsSearch } from "fumadocs-core/search/client";
 import {
   SearchDialog,
   SearchDialogClose,
-  SearchDialogContent, SearchDialogFooter,
+  SearchDialogContent,
+  SearchDialogFooter,
   SearchDialogHeader,
   SearchDialogIcon,
   SearchDialogInput,
   SearchDialogList,
   SearchDialogOverlay,
   type SharedProps,
-} from 'fumadocs-ui/components/dialog/search';
+} from "fumadocs-ui/components/dialog/search";
 import TagList from "@/components/Search/TagList";
 import TagListItem from "@/components/Search/TagListItem";
 import { useEffect, useMemo, useState } from "react";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 
 export default function SearchComponent(props: SharedProps) {
   const pathname = usePathname();
-  const t = useTranslations('Header.Search')
+  const t = useTranslations("Header.Search");
   const currentTag = useMemo(() => {
-    const segments = pathname.split('/');
-    const validTags = ['hytale', 'metrics', 'api'];
-    const foundTag = segments.find(s => validTags.includes(s));
-    return foundTag || 'minecraft';
+    const segments = pathname.split("/");
+    const validTags = ["hytale", "metrics", "api"];
+    const foundTag = segments.find((s) => validTags.includes(s));
+    return foundTag || "minecraft";
   }, [pathname]);
 
   const [tag, setTag] = useState<string | undefined>(currentTag);
@@ -34,26 +35,48 @@ export default function SearchComponent(props: SharedProps) {
   }, [currentTag]);
 
   const { search, setSearch, query } = useDocsSearch({
-    type: 'fetch',
+    type: "fetch",
     tag,
-    api: '/search',
+    api: "/search",
   });
   return (
-    <SearchDialog search={search} onSearchChange={setSearch} isLoading={query.isLoading} {...props}>
+    <SearchDialog
+      search={search}
+      onSearchChange={setSearch}
+      isLoading={query.isLoading}
+      {...props}
+    >
       <SearchDialogOverlay />
-      <SearchDialogContent className='overflow-visible'>
+      <SearchDialogContent className="overflow-visible">
         <SearchDialogHeader>
           <SearchDialogIcon />
           <SearchDialogInput />
           <SearchDialogClose />
         </SearchDialogHeader>
-        <SearchDialogList items={query.data !== 'empty' ? query.data : null} />
+        <SearchDialogList items={query.data !== "empty" ? query.data : null} />
         <SearchDialogFooter className="flex flex-row rounded-2xl py-1 px-1">
-          <TagList currentTag={tag || 'minecraft'}>
-            <TagListItem isSelect={tag == 'minecraft'} icon={'pickaxe'} title={t('Minecraft.name')} description={t('Minecraft.description')} onclick={() => setTag('minecraft')} />
-            <TagListItem isSelect={tag == 'hytale'} icon={'leaf'} title={t('Hytale.name')} description={t('Hytale.description')} onclick={() => setTag('hytale')} />
-            <TagListItem isSelect={tag == 'metrics'} icon={'cable'} title={t('Metrics.name')} description={t('Metrics.description')} onclick={() => setTag('metrics')} />
-            <TagListItem isSelect={tag == 'api'} icon={'chart-candlestick'} title={t('API.name')} description={t('API.description')} onclick={() => setTag('api')} />
+          <TagList currentTag={tag || "minecraft"}>
+            <TagListItem
+              isSelect={tag == "minecraft"}
+              icon={"pickaxe"}
+              title={t("Minecraft.name")}
+              description={t("Minecraft.description")}
+              onclick={() => setTag("minecraft")}
+            />
+            <TagListItem
+              isSelect={tag == "hytale"}
+              icon={"leaf"}
+              title={t("Hytale.name")}
+              description={t("Hytale.description")}
+              onclick={() => setTag("hytale")}
+            />
+            <TagListItem
+              isSelect={tag == "api"}
+              icon={"chart-candlestick"}
+              title={t("API.name")}
+              description={t("API.description")}
+              onclick={() => setTag("api")}
+            />
           </TagList>
         </SearchDialogFooter>
       </SearchDialogContent>
